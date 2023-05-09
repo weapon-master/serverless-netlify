0b5447df6c4b39433e2d84e8a7aab8674f771292
demon-monk <antdude@126.com>
2023-05-09 22:11:27 +0800

    3rd party api call

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,22 @@
+require('dotenv').config();
 const movies = require('../data/movies.json');
 
 exports.handler = async () => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(movies),
-    }
-}
+  const api = new URL('http://www.omdbapi.com/');
+  api.searchParams.set('apikey', process.env.OMDB_API_KEY);
+  const moviesWithScore = await Promise.all(
+    movies.map((movie) => {
+      api.searchParams.set('i', movie.id);
+      return fetch(api)
+        .then((resp) => resp.json())
+        .then((data) => ({
+          ...movie,
+          scores: data.Ratings,
+        }));
+    }),
+  );
+  return {
+    statusCode: 200,
+    body: JSON.stringify(moviesWithScore),
+  };
+};
